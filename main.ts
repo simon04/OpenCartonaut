ad18888bcb5b0e332fdab607aff5445b722ccc6d
Simon Legner <Simon.Legner@gmail.com>
2023-04-15 21:29:27 +0200

    Ctrl+Enter

--- main.ts
+++ main.ts
@@ -23,36 +23,49 @@ queryTextarea.value ||=
 mapcssTextarea.value ||=
   localStorage.getItem("overpass-ol.mapcss") || defaultMapCSS;
 
-document
-  .getElementById("executeQuery")!
-  .addEventListener("click", async function (e) {
-    try {
-      this.className = "pending";
-      await executeQuery(queryTextarea.value);
-      this.className = "success";
-      localStorage.setItem("overpass-ol.query", queryTextarea.value);
-    } catch (error) {
-      this.title = error?.message || String(error);
-      this.className = "error";
-      console.error(error);
-    }
-    document.getElementById("executeStyle")?.dispatchEvent(e);
-  });
+queryTextarea.addEventListener(
+  "keydown",
+  (e) => (e.ctrlKey || e.metaKey) && e.key === "Enter" && executeQueryClick()
+);
+mapcssTextarea.addEventListener(
+  "keydown",
+  (e) => (e.ctrlKey || e.metaKey) && e.key === "Enter" && executeStyleClick()
+);
 
-document
-  .getElementById("executeStyle")!
-  .addEventListener("click", async function () {
-    try {
-      this.className = "pending";
-      await executeStyle(mapcssTextarea.value);
-      this.className = "success";
-      localStorage.setItem("overpass-ol.mapcss", mapcssTextarea.value);
-    } catch (error) {
-      this.title = error?.message || String(error);
-      this.className = "error";
-      console.error(error);
-    }
-  });
+const executeQueryButton = document.getElementById(
+  "executeQuery"
+) as HTMLButtonElement;
+executeQueryButton.addEventListener("click", executeQueryClick);
+async function executeQueryClick() {
+  try {
+    executeQueryButton.className = "pending";
+    await executeQuery(queryTextarea.value);
+    executeQueryButton.className = "success";
+    localStorage.setItem("overpass-ol.query", queryTextarea.value);
+  } catch (error) {
+    executeQueryButton.title = error?.message || String(error);
+    executeQueryButton.className = "error";
+    console.error(error);
+  }
+  executeStyleClick();
+}
+
+const executeStyleButton = document.getElementById(
+  "executeStyle"
+) as HTMLButtonElement;
+executeStyleButton.addEventListener("click", executeStyleClick);
+async function executeStyleClick() {
+  try {
+    executeStyleButton.className = "pending";
+    await executeStyle(mapcssTextarea.value);
+    executeStyleButton.className = "success";
+    localStorage.setItem("overpass-ol.mapcss", mapcssTextarea.value);
+  } catch (error) {
+    executeStyleButton.title = error?.message || String(error);
+    executeStyleButton.className = "error";
+    console.error(error);
+  }
+}
 
 const vectorLayer = new VectorLayer({});
 
