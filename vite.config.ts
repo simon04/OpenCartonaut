3a29fc0d284661dd05e8e22b0f120ad6e3ea25b7
Simon Legner <Simon.Legner@gmail.com>
2026-05-27 22:53:02 +0200

    Migrate to vite-plus from vite
    https://viteplus.dev/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { type Plugin, defineConfig, createFilter } from "vite";
+import { type Plugin, defineConfig, createFilter } from "vite-plus";
 import _peggy from "peggy";
 
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
   build: {
     sourcemap: true,
   },
